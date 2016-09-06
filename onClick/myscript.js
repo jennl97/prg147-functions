3dcffadf4dcccecb13d0e10debd08d181cad706e
jennl97 <jennhott97@gmail.com>
2016-09-05 21:11:00 -0500

    Add files via upload

---
+++ onClick/myscript.js
@@ -0,0 +1,73 @@
+/**
+ * Created by JENN on 9/5/2016.
+ */
+function change01() {
+    document.getElementById("large").src="images/Chloe.jpg";
+}
+function myPar01() {
+    document.getElementById("myPar").innerHTML = "Meet Chloe";
+}
+
+function change02() {
+    document.getElementById("large").src="images/interior_1.jpg";
+}
+function myPar02() {
+    document.getElementById("myPar").innerHTML = "Hotel in Florida";
+}
+
+function change03() {
+    document.getElementById("large").src="images/landscape_1_1.jpg";
+}
+function myPar03() {
+    document.getElementById("myPar").innerHTML = "A field near my home";
+}
+
+function  change04() {
+    document.getElementById("large").src="images/landscape_3_1.jpg";
+}
+function myPar04() {
+    document.getElementById("myPar").innerHTML = "My old neighborhood";
+}
+
+function  change05() {
+    document.getElementById("large").src="images/Line_1.jpg";
+}
+function myPar05() {
+    document.getElementById("myPar").innerHTML = "Tayah's baritone";
+}
+
+function change06() {
+    document.getElementById("large").src="images/Line_2.jpg";
+    }
+function myPar06() {
+    document.getElementById("myPar").innerHTML = "More of the baritone";
+}
+
+function change07() {
+    document.getElementById("large").src="images/people_1.jpg";
+}
+function myPar07() {
+    document.getElementById("myPar").innerHTML = "Chloe being cute";
+}
+
+function change08() {
+    document.getElementById("large").src="images/people_3.jpg";
+}
+function myPar08() {
+    document.getElementById("myPar").innerHTML = "Chloe drawing";
+}
+
+function change09() {
+    document.getElementById("large").src="images/Window.jpg";
+}
+function myPar09() {
+    document.getElementById("myPar").innerHTML = "Through a dirty window";
+}
+
+function change10() {
+    document.getElementById("large").src="images/Sunset.jpg";
+}
+function myPar10() {
+    document.getElementById("myPar").innerHTML = "Missing that Florida sunset!";
+}
+
